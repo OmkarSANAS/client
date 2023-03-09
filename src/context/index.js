525f190ce15be2d298af591cd48ab3339a2d5859
Omkar Sanas <omkar.sanas91@gmail.com>
2023-03-09 20:51:11 +0530

    Client 09-03

---
+++ src/context/index.js
@@ -0,0 +1,84 @@
+import React, { useContext, createContext } from "react";
+
+import {
+  useAddress,
+  useContract,
+  useMetamask,
+  useContractWrite,
+} from "@thirdweb-dev/react";
+
+import { ethers } from "ethers";
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract(
+    "0xDA4dCb8c93E7b2F8648CcBFFD748153F761E85B1"
+  );
+
+  const { mutateAsync: createCampaign } = useContractWrite(
+    contract,
+    "createCampaign"
+  );
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    try {
+      const data = await createCampaign([
+        address,
+        form.title,
+        form.description,
+        form.target,
+        new Date(form.deadline).getTime(),
+        form.image,
+      ]);
+      console.log("Contract Call Success: ", data);
+    } catch (error) {
+      console.log("Contract Call Error: ", error);
+    }
+  };
+
+  const getCampaigns = async (form) => {
+    const campaigns = await contract.call("getCampaigns");
+    const parsedCampaigns = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(
+        campaign.amountCollected.toString()
+      ),
+      image: campaign.image,
+      pId: i,
+    }));
+    return parsedCampaigns;
+  };
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+    const filteredCampaigns = allCampaigns.filter(
+      (campaign) => campaign.owner === address
+    );
+    return filteredCampaigns;
+  };
+
+  return (
+    <StateContext.Provider
+      value={{
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
